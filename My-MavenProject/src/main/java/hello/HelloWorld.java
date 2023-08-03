17047e8cad5b1f37382778fcea1778ceecc7a996
Saneesh <Saneesh@DESKTOP-EJUEMR9>
2023-08-03 08:19:11 +0530

    My first commit

---
+++ My-MavenProject/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class HelloWorld 
+{
+	public void display()
+	{
+		System.out.println("HELLO WORLD");
+	}
+	public static void main(String[] args) 
+	{
+	HelloWorld hello=new HelloWorld();
+	hello.display();
+
+	}
+
+}
